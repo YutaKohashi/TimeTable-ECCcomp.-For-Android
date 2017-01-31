c43c7b6442013cdcb2adf2ca8657b5a438721506
YutaKohashi <kaerukerokero1995@gmail.com>
2017-01-31 19:12:35 +0900

    前期後期を分ける処理を追加

---
+++ app/src/main/java/jp/yuta/kohashi/esc/ui/adapter/AttendanceDivideRecyclerAdapter.java
@@ -0,0 +1,126 @@
+package jp.yuta.kohashi.esc.ui.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.List;
+
+import jp.yuta.kohashi.esc.R;
+import jp.yuta.kohashi.esc.model.AttendanceRateModel;
+import jp.yuta.kohashi.esc.model.enums.AttendanceRateType;
+
+/**
+ * Created by yutakohashi on 2017/01/30.
+ */
+
+public class AttendanceDivideRecyclerAdapter extends RecyclerView.Adapter<AttendanceDivideRecyclerAdapter.AttendanceDivideViewHolder> {
+
+    private List<AttendanceRateModel> items;
+    private Context mContext;
+    private LayoutInflater mLayoutInflater;
+
+    // タップされたときに呼び出されるメソッド
+    protected void onItemChecked(int position, @NonNull List<AttendanceRateModel> items, boolean b, AttendanceRateType type) {
+    }
+
+    public AttendanceDivideRecyclerAdapter(List<AttendanceRateModel> items, Context context) {
+        mLayoutInflater = LayoutInflater.from(context);
+        this.items = items;
+        this.mContext = context;
+    }
+
+    @Override
+    public AttendanceDivideViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = mLayoutInflater.inflate(R.layout.cell_attendance_divide, parent, false);
+        final AttendanceDivideRecyclerAdapter.AttendanceDivideViewHolder holder = new AttendanceDivideRecyclerAdapter.AttendanceDivideViewHolder(v);
+
+        holder.zenki.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                int position = holder.getAdapterPosition();
+                onItemChecked(position, items, b, AttendanceRateType.ZENKI);
+
+                if (b) {
+                    holder.kouki.setChecked(false);
+                } else {
+                    holder.zenki.setChecked(false);
+                }
+            }
+        });
+
+        holder.kouki.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                int position = holder.getAdapterPosition();
+                onItemChecked(position, items, b, AttendanceRateType.KOUKI);
+                if (b) {
+                    holder.zenki.setChecked(false);
+                } else {
+                    holder.zenki.setChecked(true);
+                }
+            }
+        });
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(AttendanceDivideViewHolder holder, int position) {
+        //教科名
+        holder.subjectName.setText(items.get(position).getSubjectName());
+
+        if (items.get(position).getType() == AttendanceRateType.ZENKI) {
+            holder.zenki.setChecked(true);
+            holder.kouki.setChecked(false);
+        } else {
+            holder.zenki.setChecked(false);
+            holder.kouki.setChecked(true);
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+
+    public void refresh() {
+        notifyDataSetChanged();
+    }
+
+    public void swap(List<AttendanceRateModel> items) {
+        this.items.clear();
+        this.items.addAll(items);
+//        notifyDataSetChanged();
+    }
+
+    public List<AttendanceRateModel> getItems() {
+        return items;
+    }
+
+    //ViewHolder
+    public static class AttendanceDivideViewHolder extends RecyclerView.ViewHolder {
+
+        public CardView container;
+        public TextView subjectName;
+        public RadioButton zenki;
+        public RadioButton kouki;
+
+        public AttendanceDivideViewHolder(View v) {
+            super(v);
+            container = (CardView) itemView.findViewById(R.id.card_view);
+            subjectName = (TextView) itemView.findViewById(R.id.subject_text_view);
+            zenki = (RadioButton) itemView.findViewById(R.id.radio_button_zenki);
+            kouki = (RadioButton) itemView.findViewById(R.id.radio_button_kouki);
+        }
+    }
+}
