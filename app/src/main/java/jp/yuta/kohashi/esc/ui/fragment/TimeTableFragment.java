655411502f9253ee9b05ba792c5da8980e59784d
YutaKohashi <kaerukerokero1995@gmail.com>
2017-02-18 17:11:30 +0900

    時間割のbottomSheetを別クラスに。builderを使用して表示するように変更 timetablefragmentのレイアウトを削除

--- app/src/main/java/jp/yuta/kohashi/esc/ui/fragment/TimeTableFragment.java
+++ app/src/main/java/jp/yuta/kohashi/esc/ui/fragment/TimeTableFragment.java
@@ -8,43 +8,26 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import java.util.List;
 
 import jp.yuta.kohashi.esc.R;
 import jp.yuta.kohashi.esc.model.TimeBlockItem;
+import jp.yuta.kohashi.esc.ui.view.TimeTableBottomSheet;
 import jp.yuta.kohashi.esc.ui.view.TimeTableView;
 import jp.yuta.kohashi.esc.util.preference.PrefUtil;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TimeTableFragment extends Fragment implements View.OnClickListener , TimeTableView.OnCellClickListener{
-
-    //BottomSheet
-    private RelativeLayout mBottomSheet;
-    private TextView mSubjectTextView;
-    private TextView mTeacherTextView;
-    private TextView mTimeTextView;
-    private Animation inAnim;
-    private Animation outAnim;
-    private Animation fadeInAnim;
-    private Animation fadeOutAnim;
-    private FrameLayout mCloseView;
+public class TimeTableFragment extends Fragment implements TimeTableView.OnCellClickListener {
 
     private TimeTableView mTimeTableView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)  {
-        View view = inflater.inflate(R.layout.fragment_time_table, container, false);
-        initView(view);
-        return view;
+                             Bundle savedInstanceState) {
+        return initView();
     }
 
     @Override
@@ -53,65 +36,35 @@ public class TimeTableFragment extends Fragment implements View.OnClickListener 
         mTimeTableView.setData(PrefUtil.loadTimeBlockList());
     }
 
-    private void initView(View view) {
+    private View initView() {
         mTimeTableView = new TimeTableView(getActivity());
         mTimeTableView.setClassRoomColor(R.color.bg_classroom_green);
         mTimeTableView.setOnCellClickListener(this);
-        ((FrameLayout) view.findViewById(R.id.container)).addView(mTimeTableView);
-
-        //BottomSheetView
-        mBottomSheet = (RelativeLayout)view.findViewById(R.id.bottom_sheet_view);
-        mBottomSheet.setVisibility(View.INVISIBLE);
-        mSubjectTextView = (TextView)view.findViewById(R.id.txt_subject);
-        mTeacherTextView = (TextView)view.findViewById(R.id.txt_teacher);
-        mTimeTextView = (TextView)view.findViewById(R.id.txt_time);
-        mCloseView = (FrameLayout)view.findViewById(R.id.close_view);
-        mCloseView.setOnClickListener(this);
-        mCloseView.setVisibility(View.INVISIBLE);
-        inAnim = AnimationUtils.loadAnimation(getContext(), R.anim.anim_bottom_sheet_in);
-        outAnim = AnimationUtils.loadAnimation(getContext(), R.anim.anim_bottom_sheet_out);
-        fadeInAnim = AnimationUtils.loadAnimation(getContext(),R.anim.anim_bottom_sheet_fade_in);
+        return mTimeTableView;
     }
 
     /**
      * セルクリック時
+     *
      * @param items
      * @param model
      */
     @Override
     public void onCellClick(List<TimeBlockItem> items, TimeBlockItem model) {
         if (TextUtils.isEmpty(model.getSubject())) return;
-        mSubjectTextView.setText(model.getSubject());
-        mTeacherTextView.setText(model.getTeacherName());
-        mTimeTextView.setText(getRoom(model.getRowNum()));
 
-        mBottomSheet.startAnimation(inAnim);
-        mBottomSheet.setVisibility(View.VISIBLE);
-        mBottomSheet.startAnimation(inAnim);
-        mCloseView.setVisibility(View.VISIBLE);
-        mCloseView.startAnimation(fadeInAnim);
+        new TimeTableBottomSheet.Builder(this)
+                .subject(model.getSubject())
+                .teacher(model.getTeacherName())
+                .time(getTime(model.getRowNum()))
+                .build()
+                .show();
     }
 
-    /**
-     * close bottomSheet
-     * @param view
-     */
-    @Override
-    public void onClick(View view) {
-        // Button For Close BottomSheet
-        if(view.getId() == R.id.close_view){
-            // require every times
-            fadeOutAnim = AnimationUtils.loadAnimation(getContext(),R.anim.anim_bottom_sheet_fade_out);
-            mBottomSheet.startAnimation(outAnim);
-            mBottomSheet.setVisibility(View.INVISIBLE);
-            mCloseView.setVisibility(View.INVISIBLE);
-            mCloseView.setAnimation(fadeOutAnim);
-        }
-    }
 
     @NonNull
-    private String getRoom(int i){
-        switch(i){
+    private String getTime(int i) {
+        switch (i) {
             case 0:
                 return getResources().getString(R.string.time_zero_class);
             case 1:
---
+++ app/src/main/java/jp/yuta/kohashi/esc/ui/view/TimeTableBottomSheet.java
@@ -0,0 +1,158 @@
+package jp.yuta.kohashi.esc.ui.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import jp.yuta.kohashi.esc.R;
+
+/**
+ * Created by yutakohashi on 2017/02/17.
+ */
+
+public class TimeTableBottomSheet extends RelativeLayout implements Animation.AnimationListener, View.OnClickListener {
+    private View view;
+    private View mCloseView;
+    TimeTableBottomSheet sheetThis;
+
+    private RelativeLayout mBottomSheetView;
+    private TextView mSubjectTextView;
+    private TextView mTeacherTextView;
+    private TextView mTimeTextView;
+
+    private Animation inAnim;
+    private Animation outAnim;
+    private Animation fadeInAnim;
+    private Animation fadeOutAnim;
+
+    private Builder builder;
+
+    public final Builder getBuilder() {
+        return builder;
+    }
+
+    public TimeTableBottomSheet(Builder builder) {
+        this(builder.context);
+        this.builder = builder;
+        sheetThis = this;
+        initView();
+    }
+
+    public TimeTableBottomSheet(Context context) {
+        super(context);
+        view = LayoutInflater.from(context).inflate(R.layout.view_time_table_bottom_sheet, this);
+    }
+
+    /**
+     * show BottomSheet
+     * <p>
+     * view : rootView
+     */
+    public void show() {
+        mBottomSheetView.startAnimation(inAnim);
+        mBottomSheetView.setVisibility(View.VISIBLE);
+        mBottomSheetView.startAnimation(inAnim);
+        mCloseView.setVisibility(View.VISIBLE);
+        mCloseView.startAnimation(fadeInAnim);
+    }
+
+    /**
+     * close BottomSheet
+     */
+    public void Dismiss() {
+        fadeOutAnim = AnimationUtils.loadAnimation(getContext(), R.anim.anim_bottom_sheet_fade_out);
+        mBottomSheetView.startAnimation(outAnim);
+        mBottomSheetView.setVisibility(View.INVISIBLE);
+        mCloseView.setVisibility(View.INVISIBLE);
+        mCloseView.setAnimation(fadeOutAnim);
+        fadeOutAnim.setAnimationListener(this);
+    }
+
+    private void initView() {
+        mSubjectTextView = (TextView) view.findViewById(R.id.txt_subject);
+        mTeacherTextView = (TextView) view.findViewById(R.id.txt_teacher);
+        mTimeTextView = (TextView) view.findViewById(R.id.txt_time);
+
+        mSubjectTextView.setText(builder.subject);
+        mTeacherTextView.setText(builder.teacher);
+        mTimeTextView.setText(builder.time);
+
+        mBottomSheetView = (RelativeLayout) view.findViewById(R.id.bottom_sheet_view);
+        mBottomSheetView.setVisibility(INVISIBLE);
+        mCloseView = view.findViewById(R.id.close_view);
+        mCloseView.setVisibility(INVISIBLE);
+        mCloseView.setOnClickListener(this);
+
+        inAnim = AnimationUtils.loadAnimation(getContext(), R.anim.anim_bottom_sheet_in);
+        outAnim = AnimationUtils.loadAnimation(getContext(), R.anim.anim_bottom_sheet_out);
+        fadeInAnim = AnimationUtils.loadAnimation(getContext(), R.anim.anim_bottom_sheet_fade_in);
+        ((ViewGroup)builder.containerViewGroup).addView(this);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        sheetThis.removeAllViews();
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Dismiss();
+    }
+
+
+    public static class Builder {
+        Context context;
+        View containerViewGroup;
+        String subject;
+        String teacher;
+        String time;
+
+        public Builder(Fragment fragment) {
+            this.context = fragment.getContext();
+            containerViewGroup = fragment.getView();
+        }
+
+        public Builder(Activity activity) {
+            this.context = activity.getApplicationContext();
+            containerViewGroup = activity.getWindow().getDecorView().findViewById(android.R.id.content);
+        }
+
+        public Builder subject(String subject) {
+            this.subject = subject;
+            return this;
+        }
+
+        public Builder teacher(String teacher) {
+            this.teacher = teacher;
+            return this;
+        }
+
+        public Builder time(String time) {
+            this.time = time;
+            return this;
+        }
+
+        public TimeTableBottomSheet build() {
+            TimeTableBottomSheet bottomSheet = new TimeTableBottomSheet(this);
+            return bottomSheet;
+        }
+    }
+
+}
