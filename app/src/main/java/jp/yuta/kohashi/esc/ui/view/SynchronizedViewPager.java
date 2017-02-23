43ef92da7a0315b2dc6527e09de95affec4c536e
YutaKohashi <kaerukerokero1995@gmail.com>
2017-02-23 15:12:05 +0900

    cording by fujie kun

--- app/src/main/java/jp/yuta/kohashi/esc/ui/view/SynchronizedViewPager.java
+++ app/src/main/java/jp/yuta/kohashi/esc/ui/view/SynchronizedViewPager.java
@@ -10,95 +10,101 @@ import android.view.ViewGroup;
  * Created by yutakohashi and Shion T. Fujie on 2017/02/08.
  */
 
+/**
+ *
+ */
 public class SynchronizedViewPager extends ViewPager {
-    private boolean touched;
+    /**
+     * receiving an action of synchronized view pager, it applies the action to the
+     * sychronized view pager initialized in constructors or the method setTargetViewPager.
+     */
     private Action1<Action1<SynchronizedViewPager>> eventInvoker;
 
-    public SynchronizedViewPager(Context context) {
-        super(context);
-    }
-
-    public void setTargetViewPager(SynchronizedViewPager customPager) {
-        eventInvoker = invoker -> { if(!touched) invokeEvent(customPager, invoker); };
-        this.addOnPageChangeListener(new FixSynchronizedViewPager( customPager));
+    public SynchronizedViewPager(Context context, SynchronizedViewPager target){
+        this(context);
+        eventInvoker = invoker -> invoker.apply(target);
         ((ViewGroup)this.getRootView()).setMotionEventSplittingEnabled(false); // disable multitouch on RootView
     }
 
-    private  void invokeEvent(SynchronizedViewPager target, Action1<SynchronizedViewPager> invoker){
-        target.setTouched(true);
-        invoker.apply(target);
-        target.setTouched(false);
+    public SynchronizedViewPager(Context context) {
+        super(context);
     }
 
     public SynchronizedViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
+    public SynchronizedViewPager setTargetViewPager(SynchronizedViewPager customPager) {
+        eventInvoker = invoker -> invoker.apply(customPager);
+        ((ViewGroup)this.getRootView()).setMotionEventSplittingEnabled(false); // disable multitouch on RootView
+        return  this;
+    }
+
+    /**
+     * Note: To prevent the recursive call, it calls a helper method onInterceptTouchEventOnly internally.
+     * @param ev
+     * @return
+     */
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        eventInvoker.apply(pager -> pager.onInterceptTouchEvent(ev));
+        eventInvoker.apply(pager -> pager.onInterceptTouchEventOnly(ev));
         return super.onInterceptTouchEvent(ev);
     }
 
+    /** This method is merely for invoking the non-overriden onInterceptTouchEvent
+     * ,namely the super class's method,instead of calling the overriden one.
+     *
+     * @param ev
+     * @return
+     */
+    public boolean onInterceptTouchEventOnly(MotionEvent ev){
+        return super.onInterceptTouchEvent(ev);
+    }
+
+
+    /**
+     * Note: To prevent the recursive call, it calls a helper method onTouchEventOnly internally.
+     * @param ev
+     * @return
+     */
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        eventInvoker.apply(pager -> pager.onTouchEvent(ev));
+        eventInvoker.apply(pager -> pager.onTouchEventOnly(ev));
         return super.onTouchEvent(ev);
     }
 
-    /**
-     * 同期ターゲットをセットする
-     * @param touched
+    /** This method is merely for invoking the non-overriden onTouchEvent
+     * ,namely the super class's method,instead of calling the overriden one.
+     *
+     * @param ev
+     * @return
      */
-    public void setTouched(boolean touched) {
-        this.touched = touched;
+    public boolean onTouchEventOnly(MotionEvent ev){
+        return super.onTouchEvent(ev);
     }
 
-    public boolean isTouched(){
-        return this.touched;
-    }
 
-    @Override
-    public void setCurrentItem(int item, boolean smoothScroll) {
-        eventInvoker.apply(pager -> pager.setCurrentItem(item,smoothScroll));
-        super.setCurrentItem(item, smoothScroll);
-    }
-
+    /**
+     * Note: To prevent the recursive call, it calls a helper method setCurrentItemOnly internally.
+     * @param item
+     */
     @Override
     public void setCurrentItem(int item) {
-        eventInvoker.apply(pager -> pager.setCurrentItem(item));
+        eventInvoker.apply(pager -> pager.setCurrentItemOnly(item));
         super.setCurrentItem(item);
     }
 
-    private class FixSynchronizedViewPager implements ViewPager.OnPageChangeListener{
-
-        ViewPager target;
-
-        FixSynchronizedViewPager(ViewPager target){
-            this.target = target;
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int state) {
-            if(state == ViewPager.SCROLL_STATE_IDLE){
-                if(SynchronizedViewPager.this.getCurrentItem() != target.getCurrentItem()){
-                    target.setCurrentItem(SynchronizedViewPager.this.getCurrentItem());
-                }
-            }
-        }
-
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
-        @Override
-        public void onPageSelected(int position) {}
+    /** This method is merely for invoking the non-overriden onTouchEvent
+     * ,namely the super class's method,instead of calling the overriden one.
+     * @param item
+     */
+    public void setCurrentItemOnly(int item){
+        super.setCurrentItem(item);
     }
 
     /**
-     *  an interface to use lambda expression.
+     * an interface to use lambda expression.
      */
-    interface Action0{
-        void apply();
-    }
 
     interface Action1<T> {
         void apply(T pager);
